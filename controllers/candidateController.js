4d40576836748d850ee5401b6cbdf0cb3bb3fbd6
Peyyala Venkata Karthik <peyyalavenkatakarthik4@gmail.com>
2026-08-01 22:17:23 +0530

    fixing compatability resume upload and resume get

--- controllers/candidateController.js
+++ controllers/candidateController.js
@@ -92,13 +92,15 @@ if (existingResume.rows.length > 0) {
   );
 
 }
-    return res.status(200).json({
+return res.status(200).json({
   success: true,
   message: "Resume uploaded successfully.",
   resume: {
-    overallScore: score.overall,
-    detectedSkills: matchedSkills,
-    missingSkills: missingSkills,
+    score: {
+      overall: score.overall,
+    },
+    matchedSkills,
+    missingSkills,
     formatting,
   },
 });
